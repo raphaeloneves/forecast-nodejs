15141b9c8cf88b09ba5080c72111da81a50d6549
Raphael Neves <raphaeloneves@outlook.com>
2021-06-04 15:06:48 +0100

    refactor: mapbox token from ENV error handler

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,6 @@
 const request = require("request");
 
-const MAPBOX_TOKEN =
-  "pk.eyJ1IjoicmFwaGFlbG9uZXZlcyIsImEiOiJja3BmZWJ6NnAyMHVjMnJsbG5sanE1OWRkIn0.Rt_1xdHXr89rZKeOvPzPgw";
+const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN
 
 const geocode = (location, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
@@ -9,8 +8,9 @@ const geocode = (location, callback) => {
   )}.json?access_token=${MAPBOX_TOKEN}&limit=1`;
 
   request.get({ url: url, json: true }, (error, response, body) => {
-    if (error) {
-      callback('error', undefined);
+    if (error || body.message) {
+      const message = error || body.message;
+      callback(message, undefined);
     } else if (body.features.length == 0) {
       callback("Location not found", undefined);
     } else {
